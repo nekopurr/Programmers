c054fe6b34dceda1f6d212f13e8bb80d3cf2cd41
nekopurr <rksk0071@naver.com>
2023-05-15 00:04:32 +0900

    분수의 덧셈

---
+++ beginner/Day2/3rd.py
@@ -0,0 +1,18 @@
+def solution(numer1, denom1, numer2, denom2):
+    denom_mul = denom1 * denom2
+    numer1 *= denom2
+    numer2 *= denom1
+
+    result = []
+    answer = []
+    result.append(numer1 + numer2)
+    result.append(denom_mul)
+
+    def gcd(a, b):
+        while b > 0:
+            a, b = b, a % b
+        return a
+
+    answer.append(result[0] // (gcd(result[0], result[1])))
+    answer.append(result[1] // (gcd(result[0], result[1])))
+    return answer
