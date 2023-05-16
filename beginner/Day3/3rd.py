74990145a54ac0ef2354ee5e795b31130e7e7d17
nekopurr <rksk0071@naver.com>
2023-05-17 01:01:01 +0900

    최빈값 구하기

---
+++ beginner/Day3/3rd.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120812
+
+def solution(array):
+    dict = {}
+
+    for i in range(max(array) + 1):
+        dict[i] = array.count(i)
+
+    dict_max = max(dict, key=dict.get)
+    dict_multiple = len([i for i, j in dict.items() if max(dict.values()) == j])
+
+    if dict_multiple > 1:
+        return -1
+    else:
+        return dict_max
+
+solution([0])
