ccb579e7c7acfd874d486e37d370c4055755a991
nekopurr <rksk0071@naver.com>
2023-05-18 23:12:16 +0900

    옷가게 할인 받기

---
+++ beginner/Day5/1st.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120818
+import math
+
+def solution(price):
+    if price >= 500000:
+        return math.floor(price * 0.8)
+    elif price >= 300000:
+        return math.floor(price * 0.9)
+    elif price >= 100000:
+        return math.floor(price * 0.95)
+    else:
+        return price
