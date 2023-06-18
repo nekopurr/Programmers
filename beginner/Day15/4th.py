3c92ac84f6a431664cd7c2c803812b33ae884dfb
nekopurr <rksk0071@naver.com>
2023-06-18 23:20:50 +0900

    약수 구하기

---
+++ beginner/Day15/4th.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120897
+
+def solution(n):
+    answer = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            answer.append(i)
+    return answer
+
+def solution2(n):
+    return [i for i in range(1, n + 1) if n % i == 0]
