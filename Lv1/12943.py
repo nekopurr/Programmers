8d35b49acc978de1b8fb25671e7fb5dd50108d4a
nekopurr <rksk0071@naver.com>
2023-08-26 23:19:42 +0900

    콜라츠 추측

---
+++ Lv1/12943.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12943
+
+def solution(num):
+    for i in range(500):
+        if num == 1:
+            return i
+        num = num / 2 if num % 2 == 0 else num * 3 + 1
+    return -1
