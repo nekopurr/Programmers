92c47fb409c542552cfe6d167f47bd332dc5dcd5
nekopurr <rksk0071@naver.com>
2023-10-01 06:04:43 +0900

    크기가 작은 부분문자열

---
+++ Lv1/147355.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/147355
+
+def solution(t, p):
+    answer = 0
+    for i in range(len(t)-len(p)+1):
+        if int(t[i:i+len(p)]) <= int(p):
+            answer += 1
+
+    return answer
