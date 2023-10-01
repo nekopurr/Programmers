f89cb303974dfa1702c5f643be00b45ebb8b880a
nekopurr <rksk0071@naver.com>
2023-10-02 06:07:22 +0900

    [1차] 캐시

---
+++ Lv2/17680.py
@@ -0,0 +1,19 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17680
+
+from collections import deque
+
+def solution(cacheSize, cities):
+    l = [''] * cacheSize
+    cache = deque(l, maxlen=cacheSize)
+    answer = 0
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            cache.append(city)
+            answer += 5
+
+    return answer
