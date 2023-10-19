d61581d8bb8e435a8f7b87faad70e07c07b17934
nekopurr <rksk0071@naver.com>
2023-10-19 23:30:05 +0900

    A 강조하기

---
+++ beginner/181874.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181874
+
+def solution(myString):
+    answer = myString.lower()
+    answer = answer.replace('a', 'A')
+    return answer
